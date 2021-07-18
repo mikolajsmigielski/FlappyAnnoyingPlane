67c61cc9d5a718418213ca449da954f271248a9a
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-07-18 17:09:34 +0200

    Create project and add animations

---
+++ Assets/Scripts/PlaneRandomCorol.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneRandomCorol : MonoBehaviour
+{
+    public string[] Animations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var index = Random.Range(0, Animations.Length - 1);
+        GetComponent<Animator>().Play(Animations[index]);
+    }
+
+    
+    
+}
---
+++ Assets/Scripts/SpriteChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float Duration = 1f;
+    void Start()
+    {
+        StartCoroutine(ChangeSpritesCorutine());
+    }
+
+    
+    IEnumerator ChangeSpritesCorutine()
+    {
+        var renderer = GetComponent<SpriteRenderer>();
+        for (int i = 0; true; i++)
+        {
+            renderer.sprite = sprites[i % sprites.Length];
+            
+            yield return new WaitForSeconds(Duration);
+        }
+    }
+}
