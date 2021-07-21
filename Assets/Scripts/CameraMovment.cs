bb857ff71d50171288f93526730fedc60be319a4
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-07-21 14:17:24 +0200

    Add CameraMovment Script
    Create script that allow camera follow players plane

---
+++ Assets/Scripts/CameraMovment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+    public Transform TracedObject;
+    public float DeltaX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(TracedObject.position.x + DeltaX, transform.position.y, transform.position.z);
+    }
+}
