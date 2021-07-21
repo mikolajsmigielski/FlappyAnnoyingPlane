335d877339a4f37694236b4461cd5d853a5b38ba
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-07-21 15:23:51 +0200

    Improved Stra object and add point counter

---
+++ Assets/Scripts/PointsCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsCounter : MonoBehaviour
+{
+    int Points = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshText();
+    }
+    public void IncrementPoints()
+    {
+        Points++;
+        RefreshText();
+    }
+    void RefreshText()
+    {
+        GetComponent<Text>().text = Points + " Points";
+    }
+
+}
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<AudioSource>().Play();
+        GetComponent<SpriteRenderer>().enabled = false;
+        FindObjectOfType<PointsCounter>().IncrementPoints();
+
+        Destroy(gameObject, 3f);
+    }
+}
