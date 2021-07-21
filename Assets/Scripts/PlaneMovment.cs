5dbadf8f8862e0892d48e989059a91dfcfff7699
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-07-21 13:35:14 +0200

    Add PlaneMovment

--- Assets/Scripts/PlaneMovment.cs
+++ Assets/Scripts/PlaneMovment.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlaneMovment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float FlyingAngle = 60f;
+    public float FallAngle = -80f;
+    public float CurrentAngle = 0f;
+    public float Speed = 2f;
     // Update is called once per frame
     void Update()
     {
-        
+        var TargetAngle = Input.GetKey(KeyCode.Space) ? FlyingAngle : FallAngle;
+        CurrentAngle = Mathf.Lerp(CurrentAngle, TargetAngle, Time.deltaTime * 2f);
+        transform.rotation = Quaternion.Euler(Vector3.forward * CurrentAngle);
+        transform.Translate(Vector3.right * Speed * Time.deltaTime);
     }
 }
