1280b6d48eed0ca25a68d22fb5e8de621d04897b
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-07-25 15:12:26 +0200

    Create menu

---
+++ Assets/Scripts/ChangeSceneWhenClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneWhenClick : MonoBehaviour
+{
+    public string SceneName;
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
