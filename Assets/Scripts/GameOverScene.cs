d6f3df3130a96765f8a02b0d7173e12fdc7e4585
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-07-25 13:31:55 +0200

    Add option to display information about new record

--- Assets/Scripts/GameOverScene.cs
+++ Assets/Scripts/GameOverScene.cs
@@ -17,26 +17,43 @@ public class GameOverScene : MonoBehaviour
     public Text Score;
     public Medal[] Medals;
     public Image medal;
+    public GameObject NewBestScore;
     // Start is called before the first frame update
     void Start()
     {
         RefreshPoints();
         RefreshMedal();
+        RefreshRecord();
     }
-
+    int GetCurrentPoints()
+    {
+        return PlayerPrefs.GetInt("CurrentPoints", 0);
+    }
     void RefreshPoints()
     {
-        var points = PlayerPrefs.GetInt("CurrentPoints", 0);
+        var points = GetCurrentPoints();
         Score.text = points + " Points!";
     }
     void RefreshMedal()
     {
-        var points = PlayerPrefs.GetInt("CurrentPoints", 0);
+        var points = GetCurrentPoints();
         medal.sprite = Medals
             .Where(Xmedal => Xmedal.MinimumPoints <= points)
             .OrderBy(Xmedal => Xmedal.MinimumPoints)
             .Last()
             .Image;
+    }
+    void RefreshRecord()
+    {
+        var currentPoints = GetCurrentPoints();
+        var recordPoints = PlayerPrefs.GetInt("RecordPoints", 0);
+
+        bool isRecord = (currentPoints > recordPoints);
+
+        if (isRecord)
+            PlayerPrefs.SetInt("RecordPoints", currentPoints);
+
+        NewBestScore.SetActive(isRecord);
     }
 
     
