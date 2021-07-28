45246e75cb747470e1bc9919e6dafb0bc3528365
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-07-28 11:18:41 +0200

    Add level generator

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public GameObject InitialBlock;
+    public GameObject[] TerrainBlocks;
+    private int BlockIndex = 0;
+    private List<GameObject> CurrenBlocks = new List<GameObject>();
+    void Start()
+    {
+       for (int i = 0; i < 4; i++)
+        {
+            GenerateBlock();
+        } 
+    }
+    void GenerateBlock()
+    {
+        var index = Random.Range(0, TerrainBlocks.Length);
+        var prefab = TerrainBlocks[index];
+        if (BlockIndex < 2)
+            prefab = InitialBlock;
+        var block = Instantiate(prefab);
+        CurrenBlocks.Add(block);
+        block.transform.position = Vector2.right * BlockIndex * 8f;
+        GetComponent<BoxCollider2D>().transform.position= Vector2.right * (BlockIndex - 2)* 8f;
+        BlockIndex++;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GenerateBlock();
+        var block = CurrenBlocks.First();
+        Destroy(block);
+        CurrenBlocks.RemoveAt(0);
+    }
+}
